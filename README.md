62a9e7bdb878a7a1d76ae23ac3ccbce739e3b53a
Jean Lucien Randrianantenaina <rjlucienaina@gmail.com>
2023-08-05 17:55:41 +0300

    Added a cru system

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # E-Library
 
-A simple CRUD for a library
+E-Library a simple online library application built with Flask. It allows users to manage a collection of books by performing the following actions
+
+## Features
+- **Create**: Users can upload books to the library.
+- **Read**: Users can view a list of available books and download them.
+- **Update**: Users can update the information of existing books in the library.
+- **Delete**: Users can remove books from the library.
+
+**Next Task**: Add User management.
--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, redirect, send_file
 from flask_sqlalchemy import SQLAlchemy  # Database
+from flask_migrate import Migrate  # handle migration
 from datetime import datetime
+from werkzeug.utils import secure_filename
+import os
+from flask_wtf import CSRFProtect
+from io import BytesIO
+
 
 app = Flask(__name__)
 # Database setting /// for relative path
+app.config['UPLOAD_FOLDER'] = "./media"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book.db'
 db = SQLAlchemy(app)
+app.secret_key = b'_53oi3uriq9pifpff;apl'
+csrf = CSRFProtect(app)
+migrate = Migrate(app, db)
 
 # A model
 
@@ -16,7 +26,7 @@ class Book(db.Model):
     author = db.Column(db.String(200), nullable=False)
     filename = db.Column(db.String(100), nullable=False)
     data = db.Column(db.LargeBinary)
-    year = db.Column(db.DateTime, default=datetime.today().strftime('%Y'))
+    year = db.Column(db.String(4))
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -29,7 +39,9 @@ class Book(db.Model):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', books=Book.query.all())
+
+# CREATE
 
 
 @app.route('/addbook', methods=['GET', 'POST'])
@@ -38,12 +50,49 @@ def addBook():
         title = request.form['title']
         file = request.files['file']
         year = request.form['year']
-        book = Book(title=title, filename=year +"_" +
-                    title, data=file.read(), year=year)
-        db.session(book)
+        author = request.form['author']
+        book = Book(title=title, filename='',
+                    data=file.read(), author=author, year=year)
+        db.session.add(book)
         db.session.commit()
-    return render_template('index.html')
+        id = book.id
+        filename = secure_filename(str(id)+file.filename)
+        book.filename = filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        db.session.commit()
+    return redirect(url_for('index'))
 
+# DOWNLOAD
+@app.route('/delete/<int:pk>')
+def delete(pk):
+    book = Book.query.filter_by(id=pk).first()
+    os.remove(os.path.join(app.config['UPLOAD_FOLDER'], book.filename))
+    db.session.delete(book)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/update/<int:pk>', methods=['GET','POST'])
+def updateBook(pk):
+    book = Book.query.get_or_404(pk)
+    if book:
+        if request.method == "POST":
+            book.title = request.form['title']
+            book.year = request.form['year']
+            book.author = request.form['author']
+            db.session.commit()
+            return redirect(url_for('index'))
+        else:
+            return render_template('update.html', book=book)
+    else:
+        return "Book not found"
+    
+@app.route('/<int:pk>')
+def download(pk):
+    print(pk)
+    book = Book.query.filter_by(id=pk).first()
+    print(book)
+    return send_file(BytesIO(book.data),
+                     download_name=book.filename, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
